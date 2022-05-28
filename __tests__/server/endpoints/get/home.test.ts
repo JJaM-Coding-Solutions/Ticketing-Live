809700d754e84cbd2d504f632133926fa14cd558
ProwlingLynx <tamezjavier@gmail.com>
2022-05-28 16:28:39 -0500

    Fixing syntax error in async keyword.

--- __tests__/server/endpoints/get/home.test.ts
+++ __tests__/server/endpoints/get/home.test.ts
@@ -5,7 +5,7 @@ describe('getChatMessages', () => {
   test('should return a Promise Object', () => {
     expect(getChatMessages()).toBeInstanceOf(Promise);
   });
-  test('should resolve to an array of objects', () async => {
+  test('should resolve to an array of objects', async () => {
     const response = await getChatMessages();
     expect(response).toBeInstanceOf(Array);
     expect(response[0]).toBeInstanceOf(Object);
